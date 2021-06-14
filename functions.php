ed8dbb28521abdf3529cde8de49e55f6ed306b45
Guglielmo Pepe <1424836+GuglielmoPepe@users.noreply.github.com>
2021-06-14 14:47:09 +0200

    Add comments to functions

--- functions.php
+++ functions.php
@@ -17,13 +17,13 @@ add_action( 'wp_enqueue_scripts', 'slick_enqueue' );
 
 // Sets up theme defaults and registers support for various WordPress features
 function slick_setup() {
-    // Adding support for featured images.
+    // Add support for featured images.
     add_theme_support( 'post-thumbnails' );
 
-    // Adding support for core block visual styles.
+    // Add support for core block visual styles.
     add_theme_support( 'wp-block-styles' );
 
-    // Adding support for responsive embedded content.
+    // Add support for responsive embedded content.
     add_theme_support( 'responsive-embeds' );
 
     // Add support for editor styles.
@@ -41,9 +41,11 @@ function slick_setup() {
 // Enqueue scripts and styles
 function slick_enqueue() {
 
+    // Add quick.css 
     wp_register_style( 'quick', 'https://cdn.jsdelivr.net/gh/GuglielmoPepe/quick@2.0.0/src/quick.min.css', array(), '2.0.0' );
     wp_enqueue_style('quick');
 
+    // Add style.css
     wp_enqueue_style( 'slick', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 
