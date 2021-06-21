cd0373df6969176dc06ff58f0ea4985143717812
Guglielmo Pepe <1424836+GuglielmoPepe@users.noreply.github.com>
2021-06-21 16:26:23 +0200

    Add block template parts

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@ add_action( 'wp_enqueue_scripts', 'slick_enqueue' );
 
 // Sets up theme defaults and registers support for various WordPress features
 function slick_setup() {
+
     // Add support for featured images.
     add_theme_support( 'post-thumbnails' );
 
@@ -44,9 +45,9 @@ function slick_enqueue() {
 
     // Add quick.css 
     wp_register_style( 'quick', 'https://cdn.jsdelivr.net/gh/GuglielmoPepe/quick@2.0.0/src/quick.min.css', array(), '2.0.0' );
-    wp_enqueue_style('quick');
+    // wp_enqueue_style('quick');
 
     // Add style.css
-    wp_enqueue_style( 'slick', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+    // wp_enqueue_style( 'slick', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 
