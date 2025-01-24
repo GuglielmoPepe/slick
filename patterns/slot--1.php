223a23af9985a3b6772db68a390c8ba71e5ec8f8
GuglielmoPepe <1424836+GuglielmoPepe@users.noreply.github.com>
2025-01-24 17:08:15 +0100

    Add 'box--1' pattern

--- patterns/slot--1.php
+++ patterns/slot--1.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Slot
- * Slug: slick/slot
+ * Slug: slick/slot--1
  * Categories: generic
  */
 ?>
@@ -9,9 +9,9 @@
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|l","margin":{"top":"var:preset|spacing|none","bottom":"var:preset|spacing|none"},"padding":{"right":"0","left":"0","top":"var:preset|spacing|none","bottom":"var:preset|spacing|none"}},"dimensions":{"minHeight":"100%"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between"}} -->
 <div class="wp-block-group" style="min-height:100%;margin-top:var(--wp--preset--spacing--none);margin-bottom:var(--wp--preset--spacing--none);padding-top:var(--wp--preset--spacing--none);padding-right:0;padding-bottom:var(--wp--preset--spacing--none);padding-left:0">
 
-    <!-- wp:pattern {"slug":"slick/box"} /-->
+    <!-- wp:pattern {"slug":"slick/box--1"} /-->
 
-    <!-- wp:pattern {"slug":"slick/box"} /-->
+    <!-- wp:pattern {"slug":"slick/box--1"} /-->
 
 </div>
 <!-- /wp:group -->
